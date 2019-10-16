264f9d7df40f29257b2d0da73b64d306daa089cf
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-16 14:13:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,7 +19,7 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  def input_to_index=(input)
+  def input_to_index(input)
     @index = input.to_i - 1
   end
 end
