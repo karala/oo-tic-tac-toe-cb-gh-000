a9d28c2cfb3c7b23179811593366e04647e77f0d
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-16 14:31:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,11 +41,11 @@ class TicTacToe
     # puts "Please enter 1-9:"
     @user_input = gets.strip
     @index = input_to_index(@user_input)
-    if valid_move?()
-      move(board, index, current_player(board))
-      display_board(board)
-    else
-      turn(board)
-    end
+    # if valid_move?()
+    #   move(board, index, current_player(board))
+    #   display_board(board)
+    # else
+    #   turn(board)
+    # end
   end
 end
