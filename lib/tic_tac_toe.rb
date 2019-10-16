9277dfdf543956809abce3eb6be66c430ef6a976
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-16 14:39:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,11 @@ class TicTacToe
       turn()
     end
     def won?
-
+      WIN_COMBINATIONS.detect do |combo|
+        board[combo[0]] == board[combo[1]] &&
+        board[combo[1]] == board[combo[2]] &&
+        position_taken?(board, combo[0])
+      end
     end
   end
 end
